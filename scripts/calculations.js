2343e53b8f7421826b5b055dcf3e3a3d68299abf
Vinicius Bispo <bispov19@gmail.com>
2026-04-24 10:41:10 -0300

    "subindo projeto para o github"

---
+++ scripts/calculations.js
@@ -0,0 +1,18 @@
+let budgetValue = [];
+let totalExpensesValue = [];
+let expenseEntries = [
+    ["groceries", 33],
+    ["restaurants", 50],
+    ["transport", 30],
+    ["home", 70],
+    ["subscriptions", 14],
+    ["groceries", 28],
+    ["subscriptions", 12]
+];
+
+let totalExpensesValue = 0;
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
