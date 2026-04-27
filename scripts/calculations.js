4f135ac442f9afc6c61f1516b380f17fcb0df76c
Vinicius Bispo <bispov19@gmail.com>
2026-04-27 20:44:44 -0300

    "atualização calculation.js"

--- scripts/calculations.js
+++ scripts/calculations.js
@@ -1,6 +1,6 @@
 let budgetValue = [];
-let totalExpensesValue = [];
-let expenseEntries = [
+let totalExpensesValue = 0;
+const expenseEntries = [
     ["groceries", 33],
     ["restaurants", 50],
     ["transport", 30],
@@ -9,10 +9,56 @@ let expenseEntries = [
     ["groceries", 28],
     ["subscriptions", 12]
 ];
+let balanceColor = "green";
 
-let totalExpensesValue = 0;
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// CALCULO DESPESA TOTAL
+
+for (let i = 0; i < expenseEntries.length; i++) {
+    totalExpensesValue += expenseEntries[i][1];
     
 }
+
+// CALCULO DESPESA MÉDIA
+
+function calculateAverageExpense () {
+    if (expenseEntries.length === 0) {
+        return 0;
+    }
+    let averageExpense = totalExpensesValue / expenseEntries.length
+    return averageExpense;
+}
+
+// CALCULO SALDO
+
+function calculateBalance () {
+    return budgetValue - totalExpensesValue
+}
+
+
+// COR DO SALDO
+
+function updateBalanceColor () {
+    if (calculateBalance() < 0) {
+        balanceColor = "red";
+    } else if (calculateBalance() < budgetValue * 0.25) {
+        balanceColor = "orange";
+    } else {
+        balanceColor = "green";
+    }
+}
+
+// ESTATIÍSTICA POR CATEGORIA
+
+function calculateCategoryExpenses() {
+    
+}
+
+
+
+// CONSOLES
+
+console.log("Somatoria da Despesa Total", totalExpensesValue);
+console.log("Somatoria da Despesa Média", calculateAverageExpense());
+console.log("Saldo Total", calculateBalance());
+console.log("Cor do Saldo", updateBalanceColor());
